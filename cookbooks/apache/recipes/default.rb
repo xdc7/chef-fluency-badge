638fd77de16c12df34252a3333aaba30063551f1
Juzer Ali <juzerali@gmail.com>
2017-06-11 10:30:10 -0500

    added motd and platform check

--- cookbooks/apache/recipes/default.rb
+++ cookbooks/apache/recipes/default.rb
@@ -5,13 +5,19 @@
 # Copyright (c) 2017 The Authors, All Rights Reserved.
 
 
+if node['platform_family'] == 'rhel'
+  package = 'httpd'
+elsif node['platform_family'] == 'debian'
+  package = 'apache2'
+end
+
 package 'apache2' do
-  package_name 'httpd'
+  package_name package
   action :install
 end
 
 service 'apache2' do
-  service_name 'httpd'
+  service_name package
   action [:start, :enable]
 end
 
